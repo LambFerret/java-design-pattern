0d4b50c426e196be57d417b9ad2ac0d876e9d584
LambFerret <dignrjrl12@gmail.com>
2022-11-11 00:20:57 +0900

    delete Adaptor

--- src/main/java/com/lambferret/design_pattern/partterns/observer/ForecastDisplay.java
+++ src/main/java/com/lambferret/design_pattern/partterns/observer/ForecastDisplay.java
@@ -18,6 +18,7 @@ public class ForecastDisplay implements Observer, DisplayElement{
     public void introduce() {
         System.out.println("제이름은 Forecast");
     }
+
     @Override
     public void display() {
         System.out.printf("Forecast 현재온도 : %f, 습도 : %f \n", temp, humidity);
